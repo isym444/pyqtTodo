4f4479dce826c18e14bc41a2747ffdc693f2d106
Yuki Ise <70761052+Benjamin-taro@users.noreply.github.com>
2024-10-16 17:29:56 +0100

    feature of .app mac executable code to pipiline

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+APP = ['main.py']  # エントリーポイントとなるPythonスクリプトを指定します
+DATA_FILES = []    # 必要なデータファイルをリスト形式で指定します（画像、設定ファイルなど）
+OPTIONS = {
+    'argv_emulation': True,          # コマンドライン引数のサポートを有効化
+    'packages': ['PyQt5'],           # 使用するPythonパッケージ
+    'includes': ['sip'],             # その他に含めたいパッケージ
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],       # py2appをビルドプロセスで使うため指定
+)
